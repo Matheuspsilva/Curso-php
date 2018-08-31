5f1145c84f9ae35bf15bc5dce7d8f93f6b474a76
Matheuspsilva <matheuspsilva29@gmail.com>
2018-08-31 18:48:05 -0300

    Commit da Session 1

---
+++ comments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    #One line comment
+    
+    //One line Comment
+    
+    /*Multiple 
+    line 
+    comment
+    */
+    
+    
+    ?>
+    
+    
+    
+    
+</body>
+</html>
